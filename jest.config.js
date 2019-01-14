8ece3dbd372689f105e36baaebd81d7bd7c64407
Michał Żakowski <ckrewone@o2.pl>
2019-01-14 21:37:19 +0100

    Fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,22 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    "globals": {
+        "ts-jest": {
+            "tsConfig": "tsconfig.json",
+            "diagnostics": false
+        }
+    },
+    "testMatch": [
+        "**/__tests__/*.+(ts|tsx|js)"
+    ],
+    "collectCoverage": true,
+    "collectCoverageFrom" : ['src/**/*.{ts}'],
 };
+
